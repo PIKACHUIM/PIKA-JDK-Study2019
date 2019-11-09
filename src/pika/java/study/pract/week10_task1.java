0a8067cbda795fe0a8edfead741b7cd0cdc9eaea
PIKACHUIM <40362270+PIKACHUIM@users.noreply.github.com>
2019-11-09 22:33:18 +0800

    update_old_week10

---
+++ src/pika/java/study/pract/week10_task1.java
@@ -0,0 +1,30 @@
+package pika.java.study.pract;
+
+public class week10_task1 {
+    public void main(String[] args)
+    {
+
+    }
+}
+
+class queue
+{
+    int[] element;
+    int   szie;
+    public queue(int[] inpu)
+    {
+
+    }
+    public void dequeue(int v)
+    {
+
+    }
+    public bool empty()
+    {
+
+    }
+    public getsize()
+    {
+        return
+    }
+}
